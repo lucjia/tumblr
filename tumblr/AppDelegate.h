8f3d5f18286198fd72a46b610f608b0c745814d1
Lucy Jiang <lucjia@fb.com>
2019-06-27 10:28:30 -0700

    Initial Commit

---
+++ tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tumblr
+//
+//  Created by lucjia on 6/27/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tumblr
+//
+//  Created by lucjia on 6/27/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
